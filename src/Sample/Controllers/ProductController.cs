135b91000c281c111cc4eff1db40f12fd540d65f
Erik Juhlin <erik.juhlin@tretton37.com>
2018-04-04 07:58:02 +0200

    Sample site

---
+++ src/Sample/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sample.Controllers
+{
+    [Route("[controller]")]
+    public class ProductController : Controller
+    {
+        [HttpGet("{id}")]
+        [ResponseCache(Duration = 60*30)]
+        public IActionResult Index(int id)
+        {
+            var product = new Product
+            {
+                Id = id,
+                Title = $"Title {id}",
+                Description = "Lorem ipsum"
+            };
+            return PartialView(product);
+        }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
