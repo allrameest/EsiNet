ec593f2a0c7f5df8d2e9ba479bd1ce494b20e212
Erik Juhlin <erik.juhlin@tretton37.com>
2018-04-17 14:30:12 +0200

    Benchmarks

---
+++ src/Tests/Benchmarks.cs
@@ -0,0 +1,166 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using EsiNet;
+using EsiNet.AspNetCore;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Net.Http.Headers;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Tests
+{
+    public class Benchmarks
+    {
+        private readonly ITestOutputHelper _output;
+
+        public Benchmarks(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public async Task SimpleCached()
+        {
+            var urlContentMap = new Dictionary<string, (string, int?)>
+            {
+                ["/"] = (@"...<esi:include src=""/header""/>...", 60),
+                ["/header"] = ("HEADER", 3600)
+            };
+
+            await BenchmarkEsi(urlContentMap);
+        }
+
+        [Fact]
+        public async Task Nested3LevelsCached()
+        {
+            var urlContentMap = Create3LevelsOfUrls(5, 5, 5);
+
+            await BenchmarkEsi(urlContentMap);
+        }
+
+        [Fact]
+        public async Task Nested3LevelsNoCache()
+        {
+            var urlContentMap = Create3LevelsOfUrls(5, 5, 5, null);
+
+            await BenchmarkEsi(urlContentMap);
+        }
+
+        private async Task<string> BenchmarkEsi(Dictionary<string, (string, int?)> urlContentMap)
+        {
+            var cache = CreateCache();
+            var parser = CreateParser(cache, urlContentMap);
+
+            return await Benchmark(async () =>
+            {
+                var (rootContent, rootCache) = urlContentMap["/"];
+
+                IEsiFragment fragment;
+                if (rootCache.HasValue)
+                {
+                    fragment = await cache.GetOrAdd("/",
+                        () =>
+                        {
+                            var esiFragment = parser.Parse(rootContent);
+                            var result = (esiFragment, TimeSpan.FromSeconds(rootCache.GetValueOrDefault()));
+                            return Task.FromResult(result);
+                        });
+                }
+                else
+                {
+                    fragment = parser.Parse(rootContent);
+                }
+
+                return await fragment.Execute();
+            });
+        }
+
+        private static EsiFragmentCache CreateCache()
+        {
+            return new EsiFragmentCache(
+                new MemoryCache(
+                    new MemoryCacheOptions()));
+        }
+
+        private static EsiBodyParser CreateParser(
+            EsiFragmentCache cache, Dictionary<string, (string, int?)> urlContentMap)
+        {
+            return EsiParserFactory.Create(
+                cache,
+                new FakeStaticHttpLoader(urlContentMap));
+        }
+
+        private async Task<T> Benchmark<T>(Func<Task<T>> action, int count = 1000)
+        {
+            // Warmup
+            var result = await action();
+
+            var sw = Stopwatch.StartNew();
+            for (var i = 0; i < count; i++)
+            {
+                await action();
+            }
+            sw.Stop();
+            _output.WriteLine($"{Math.Round(sw.Elapsed.TotalMilliseconds, 3)} ms");
+
+            return result;
+        }
+
+        private static Dictionary<string, (string, int?)> Create3LevelsOfUrls(int level1Count, int level2Count, int level3Count, int? maxAge = 60)
+        {
+            var urlContentMap = new Dictionary<string, (string, int?)>();
+
+            var root = new List<string>();
+            for (int i = 0; i < level1Count; i++)
+            {
+                var lvl1 = new List<string>();
+
+                for (int j = 0; j < level2Count; j++)
+                {
+                    var lvl2 = new List<string>();
+
+                    for (int k = 0; k < level3Count; k++)
+                    {
+                        var lvl3Url = $"/{i}/{j}/{k}";
+                        lvl2.Add($@"<esi:include src=""{lvl3Url}""/>");
+                        urlContentMap[lvl3Url] = ($"fragment({lvl3Url})", maxAge);
+                    }
+
+                    var lvl2Url = $"/{i}/{j}";
+                    lvl1.Add($@"<esi:include src=""{lvl2Url}""/>");
+                    urlContentMap[lvl2Url] = (string.Join(">>>>>", lvl2), maxAge);
+                }
+
+                var lvl1Url = $"/{i}";
+                root.Add($@"<esi:include src=""{lvl1Url}""/>");
+                urlContentMap[lvl1Url] = (string.Join("#####", lvl1), maxAge);
+            }
+
+            urlContentMap["/"] = (string.Join("-----", root), maxAge);
+            return urlContentMap;
+        }
+    }
+
+    public class FakeStaticHttpLoader : IHttpLoader
+    {
+        private readonly Dictionary<string, (string, int?)> _urlContentMap;
+
+        public FakeStaticHttpLoader(Dictionary<string, (string, int?)> urlContentMap)
+        {
+            _urlContentMap = urlContentMap;
+        }
+
+        public Task<HttpResponseMessage> Get(string url)
+        {
+            var (content, maxAge) = _urlContentMap[url];
+            var response = new HttpResponseMessage(HttpStatusCode.OK) {Content = new StringContent(content)};
+            response.Headers.Add(HeaderNames.CacheControl, maxAge.HasValue ? $"public,max-age={maxAge.Value}" : "private");
+
+            return Task.FromResult(response);
+        }
+    }
+}
