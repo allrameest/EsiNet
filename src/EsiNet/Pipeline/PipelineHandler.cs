ef38576d5de6c3fd11b03038d5f47adb4f23690c
Erik Juhlin <erik.juhlin@tretton37.com>
2018-04-20 23:39:19 +0200

    Pipeline instances cache

--- src/EsiNet/Pipeline/PipelineHandler.cs
+++ src/EsiNet/Pipeline/PipelineHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using EsiNet.Fragments;
@@ -16,6 +17,9 @@ namespace EsiNet.Pipeline
     public class PipelineHandler<T> : PipelineHandler
         where T : IEsiFragment
     {
+        private readonly object _syncRoot = new object();
+        private IReadOnlyCollection<IFragmentExecutePipeline<T>> _pipelinesCache;
+
         public override async Task<string> Handle(
             ServiceFactory serviceFactory,
             IEsiFragment fragment,
@@ -25,13 +29,32 @@ namespace EsiNet.Pipeline
 
             Task<string> ExecuteHandler(T f) => executor(f);
 
-            var pipelines = serviceFactory.GetInstances<IFragmentExecutePipeline<T>>();
+            var pipelines = GetPipelines(serviceFactory);
 
             return await pipelines
                 .Reverse()
                 .Aggregate(
                     (ExecuteDelegate<T>) ExecuteHandler,
                     (next, pipeline) => async f => await pipeline.Handle(f, next))(typedFragment);
+        }
+
+        private IEnumerable<IFragmentExecutePipeline<T>> GetPipelines(ServiceFactory serviceFactory)
+        {
+            var pipelines = _pipelinesCache;
+            if (pipelines == null)
+            {
+                lock (_syncRoot)
+                {
+                    pipelines = _pipelinesCache;
+                    if (pipelines == null)
+                    {
+                        pipelines = serviceFactory.GetInstances<IFragmentExecutePipeline<T>>().ToArray();
+                        _pipelinesCache = pipelines;
+                    }
+                }
+            }
+
+            return pipelines;
         }
     }
 }
