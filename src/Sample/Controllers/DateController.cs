513c3b87bcac7614a2385c82475bd3917bb516ee
Erik Juhlin <erik.juhlin@tretton37.com>
2018-04-21 10:21:16 +0200

    Date fragment with lower cache time than parent

---
+++ src/Sample/Controllers/DateController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sample.Controllers
+{
+    [Route("[controller]")]
+    public class DateController : Controller
+    {
+        [HttpGet]
+        [ResponseCache(Duration = 10)]
+        public IActionResult Index()
+        {
+            return Content(DateTime.Now.ToString());
+        }
+    }
+}
