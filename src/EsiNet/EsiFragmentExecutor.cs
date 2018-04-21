89664835ca70dc849025e90c333a0bdbec765dfe
Erik Juhlin <erik.juhlin@tretton37.com>
2018-04-21 20:59:45 +0200

    Performance optimization of pipelines

--- src/EsiNet/EsiFragmentExecutor.cs
+++ src/EsiNet/EsiFragmentExecutor.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using EsiNet.Fragments;
 using EsiNet.Pipeline;
@@ -8,6 +10,8 @@ namespace EsiNet
 {
     public class EsiFragmentExecutor
     {
+        private readonly ConcurrentDictionary<Type, IReadOnlyCollection<PipelineDelegate>> _pipelineCache =
+            new ConcurrentDictionary<Type, IReadOnlyCollection<PipelineDelegate>>();
         private readonly IReadOnlyDictionary<Type, Func<IEsiFragment, Task<string>>> _executors;
         private readonly ServiceFactory _serviceFactory;
 
@@ -19,7 +23,7 @@ namespace EsiNet
             _serviceFactory = serviceFactory;
         }
 
-        public Task<string> Execute(IEsiFragment fragment)
+        public async Task<string> Execute(IEsiFragment fragment)
         {
             var fragmentType = fragment.GetType();
             if (!_executors.TryGetValue(fragmentType, out var executor))
@@ -27,8 +31,20 @@ namespace EsiNet
                 throw new NotSupportedException($"No executor found for type '{fragmentType}'.");
             }
 
-            var pipelineHandler = PipelineHandlerFactory.Create(fragmentType);
-            return pipelineHandler.Handle(_serviceFactory, fragment, executor);
+            var pipelineDelegates = _pipelineCache.GetOrAdd(fragmentType, GetPipelineDelegates);
+
+            Task<string> Exec(IEsiFragment f) => executor(f);
+
+            return await pipelineDelegates
+                .Aggregate(
+                    (ExecuteDelegate) Exec,
+                    (next, pipeline) => async f => await pipeline(f, next))(fragment);
+        }
+
+        private IReadOnlyCollection<PipelineDelegate> GetPipelineDelegates(Type fragmentType)
+        {
+            var pipelineService = PipelineServiceFactory.Create(fragmentType);
+            return pipelineService.GetPipelineDelegates(_serviceFactory);
         }
     }
 }
---
+++ src/EsiNet/Pipeline/PipelineResolver.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EsiNet.Fragments;
+
+namespace EsiNet.Pipeline
+{
+    public class PipelineResolver<T> : IPipelineResolver
+        where T : IEsiFragment
+    {
+        public IReadOnlyCollection<PipelineDelegate> GetPipelineDelegates(
+            ServiceFactory serviceFactory)
+        {
+            var pipelines = serviceFactory.GetInstances<IFragmentExecutePipeline<T>>();
+
+            return pipelines
+                .Reverse()
+                .Select(pipeline => new PipelineDelegate((fragment, next) =>
+                {
+                    return pipeline.Handle((T) fragment, TypedNext);
+
+                    Task<string> TypedNext(T f) => next(f);
+                }))
+                .ToArray();
+        }
+    }
+}
