3ac2349554ec96ffd2c917a0e1d9bc2f5df073bb
Erik Juhlin <erik.juhlin@tretton37.com>
2018-04-24 12:40:08 +0200

    Null cache

---
+++ src/EsiNet/Caching/NullEsiFragmentCache.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using EsiNet.Fragments;
+
+namespace EsiNet.Caching
+{
+    public class NullEsiFragmentCache : IEsiFragmentCache
+    {
+        public async Task<IEsiFragment> GetOrAdd(string url, Func<Task<(IEsiFragment, CacheControlHeaderValue)>> valueFactory)
+        {
+            var (fragment, _) = await valueFactory();
+            return fragment;
+        }
+    }
+}
