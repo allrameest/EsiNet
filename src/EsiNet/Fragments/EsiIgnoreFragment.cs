d64306ef62cc7bada41ad998f0da4a3525443b22
Erik Juhlin <erik.juhlin@tretton37.com>
2018-05-03 21:14:44 +0200

    Removed unused code

--- src/EsiNet/Fragments/EsiIgnoreFragment.cs
+++ src/EsiNet/Fragments/EsiIgnoreFragment.cs
@@ -1,14 +1,9 @@
 ﻿using System;
-using System.Threading.Tasks;
 
 namespace EsiNet.Fragments
 {
     [Serializable]
     public class EsiIgnoreFragment : IEsiFragment
     {
-        public Task<string> Execute()
-        {
-            return Task.FromResult(string.Empty);
-        }
     }
 }
