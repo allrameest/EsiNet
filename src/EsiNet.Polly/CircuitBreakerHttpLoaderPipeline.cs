7401d3c488409cd5e609a6df8e29f0409531a41e
Erik Juhlin <erik.juhlin@tretton37.com>
2018-05-04 22:45:48 +0200

    Circuit breaker pipeline

---
+++ src/EsiNet.Polly/CircuitBreakerHttpLoaderPipeline.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using EsiNet.Logging;
+using EsiNet.Pipeline;
+using Polly;
+using Polly.CircuitBreaker;
+
+namespace EsiNet.Polly
+{
+    public class CircuitBreakerHttpLoaderPipeline : IHttpLoaderPipeline
+    {
+        private readonly CircuitBreakerPolicy _breakerPolicy;
+
+        public CircuitBreakerHttpLoaderPipeline(
+            Log log,
+            int exceptionsAllowedBeforeBreaking,
+            TimeSpan durationOfBreak)
+        {
+            _breakerPolicy = Policy
+                .Handle<HttpRequestException>()
+                .Or<TaskCanceledException>()
+                .CircuitBreakerAsync(
+                    exceptionsAllowedBeforeBreaking,
+                    durationOfBreak,
+                    (exception, time) =>
+                    {
+                        var message =
+                            $"Error occured and circuit breaker will block calls for {time.TotalSeconds:0} seconds";
+                        log.Error(() => message, exception);
+                    },
+                    () => { log.Information(() => "Circuit breaker restored"); });
+        }
+
+        public Task<HttpResponseMessage> Handle(string url, HttpLoadDelegate next)
+        {
+            return _breakerPolicy.ExecuteAsync(() => next(url));
+        }
+    }
+}
