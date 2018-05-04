d1b1b5048a055e40dcb3597f6a481a82a314c701
Erik Juhlin <erik.juhlin@tretton37.com>
2018-05-04 22:46:05 +0200

    Retry pipeline

---
+++ src/EsiNet.Polly/RetryHttpLoaderPipeline.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using EsiNet.Logging;
+using EsiNet.Pipeline;
+using Polly;
+using Polly.Retry;
+
+namespace EsiNet.Polly
+{
+    public class RetryHttpLoaderPipeline : IHttpLoaderPipeline
+    {
+        private readonly RetryPolicy _retryPolicy;
+
+        public RetryHttpLoaderPipeline(Log log, int retryCount)
+        {
+            _retryPolicy = Policy
+                .Handle<HttpRequestException>()
+                .RetryAsync(retryCount,
+                    (exception, _) => { log.Information(() => "Error making http call. Retrying.", exception); });
+        }
+
+        public Task<HttpResponseMessage> Handle(string url, HttpLoadDelegate next)
+        {
+            return _retryPolicy.ExecuteAsync(() => next(url));
+        }
+    }
+}
