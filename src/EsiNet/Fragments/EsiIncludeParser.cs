16fd31db1488b5cf7131d8b38e08ac30376234e0
Erik Juhlin <erik.juhlin@tretton37.com>
2018-05-05 09:31:56 +0200

    Handle alt and onerror attributes

--- src/EsiNet/Fragments/EsiIncludeParser.cs
+++ src/EsiNet/Fragments/EsiIncludeParser.cs
@@ -7,9 +7,31 @@ namespace EsiNet.Fragments
     {
         public IEsiFragment Parse(IReadOnlyDictionary<string, string> attributes, string body)
         {
-            var src = attributes["src"];
+            var srcUrl = attributes["src"];
 
-            return new EsiIncludeFragment(new Uri(src, UriKind.RelativeOrAbsolute));
+            var srcFragment = new EsiIncludeFragment(new Uri(srcUrl, UriKind.RelativeOrAbsolute));
+
+            IEsiFragment includeFragment;
+            if (attributes.TryGetValue("alt", out var altUrl))
+            {
+                var altFragment = new EsiIncludeFragment(new Uri(altUrl, UriKind.RelativeOrAbsolute));
+                includeFragment = new EsiTryFragment(srcFragment, altFragment);
+            }
+            else
+            {
+                includeFragment = srcFragment;
+            }
+
+            return ShouldContinueOnError(attributes)
+                ? new EsiTryFragment(includeFragment, new EsiIgnoreFragment())
+                : includeFragment;
+        }
+
+        private static bool ShouldContinueOnError(IReadOnlyDictionary<string, string> attributes)
+        {
+            return
+                attributes.TryGetValue("onerror", out var onerrorValue) &&
+                onerrorValue.Equals("continue", StringComparison.OrdinalIgnoreCase);
         }
     }
 }
