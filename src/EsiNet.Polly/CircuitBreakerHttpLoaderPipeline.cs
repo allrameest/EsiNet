2c2b581eed4367e65d34631317325977bf4775e3
Erik Juhlin <erik.juhlin@tretton37.com>
2018-05-05 08:50:29 +0200

    Policy per url, host or similar

--- src/EsiNet.Polly/CircuitBreakerHttpLoaderPipeline.cs
+++ src/EsiNet.Polly/CircuitBreakerHttpLoaderPipeline.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Net.Http;
 using System.Threading.Tasks;
 using EsiNet.Logging;
@@ -10,31 +11,47 @@ namespace EsiNet.Polly
 {
     public class CircuitBreakerHttpLoaderPipeline : IHttpLoaderPipeline
     {
-        private readonly CircuitBreakerPolicy _breakerPolicy;
+        private readonly Log _log;
+        private readonly int _exceptionsAllowedBeforeBreaking;
+        private readonly TimeSpan _durationOfBreak;
+        private readonly Func<Uri, string> _breakerKeyFactory;
+
+        private readonly ConcurrentDictionary<string, CircuitBreakerPolicy> _breakerPolicies =
+            new ConcurrentDictionary<string, CircuitBreakerPolicy>();
 
         public CircuitBreakerHttpLoaderPipeline(
             Log log,
             int exceptionsAllowedBeforeBreaking,
-            TimeSpan durationOfBreak)
+            TimeSpan durationOfBreak,
+            Func<Uri, string> breakerKeyFactory = null)
         {
-            _breakerPolicy = Policy
-                .Handle<HttpRequestException>()
-                .Or<TaskCanceledException>()
-                .CircuitBreakerAsync(
-                    exceptionsAllowedBeforeBreaking,
-                    durationOfBreak,
-                    (exception, time) =>
-                    {
-                        var message =
-                            $"Error occured and circuit breaker will block calls for {time.TotalSeconds:0} seconds";
-                        log.Error(() => message, exception);
-                    },
-                    () => { log.Information(() => "Circuit breaker restored"); });
+            _log = log;
+            _exceptionsAllowedBeforeBreaking = exceptionsAllowedBeforeBreaking;
+            _durationOfBreak = durationOfBreak;
+            _breakerKeyFactory = breakerKeyFactory ?? (uri => uri.ToString());
         }
 
         public Task<HttpResponseMessage> Handle(Uri uri, HttpLoadDelegate next)
         {
-            return _breakerPolicy.ExecuteAsync(() => next(uri));
+            var breakerPolicy = _breakerPolicies.GetOrAdd(_breakerKeyFactory(uri), _ => CreatePolicy());
+            return breakerPolicy.ExecuteAsync(() => next(uri));
+        }
+
+        private CircuitBreakerPolicy CreatePolicy()
+        {
+            return Policy
+                .Handle<HttpRequestException>()
+                .Or<TaskCanceledException>()
+                .CircuitBreakerAsync(
+                    _exceptionsAllowedBeforeBreaking,
+                    _durationOfBreak,
+                    (exception, time) =>
+                    {
+                        var message =
+                            $"Error occured and circuit breaker will block calls for {time.TotalSeconds:0} seconds";
+                        _log.Error(() => message, exception);
+                    },
+                    () => { _log.Information(() => "Circuit breaker restored"); });
         }
     }
 }
