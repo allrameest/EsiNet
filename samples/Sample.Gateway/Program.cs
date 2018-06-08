e3d3402c4d72db108ec28302413fd3c1cb32e2f5
Erik Juhlin <erik.juhlin@tretton37.com>
2018-06-08 10:23:16 +0200

    Sample gateway using ocelot

---
+++ samples/Sample.Gateway/Program.cs
@@ -0,0 +1,38 @@
+﻿using EsiNet.AspNetCore;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace Sample.Gateway
+{
+    public class Program
+    {
+        public static void Main(string[] args) => BuildWebHost(args).Run();
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureServices(services =>
+                {
+                    services.AddDistributedMemoryCache();
+                    services.AddOcelot();
+                    services.AddEsiNet();
+                })
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config
+                        .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                        .AddJsonFile("ocelot.json")
+                        .AddEnvironmentVariables();
+                })
+                .Configure(app =>
+                {
+                    app.UseEsiNet();
+                    app.UseOcelot().Wait();
+                })
+                .Build();
+    }
+}
