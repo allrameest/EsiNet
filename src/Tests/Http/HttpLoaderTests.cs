7a97b386d68c3036e3d4648e3200dac073c495ce
Erik Juhlin <erik.juhlin@tretton37.com>
2018-09-12 10:48:42 +0200

    Null log in tests

--- src/Tests/Http/HttpLoaderTests.cs
+++ src/Tests/Http/HttpLoaderTests.cs
@@ -4,6 +4,7 @@ using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using EsiNet.Http;
+using EsiNet.Logging;
 using EsiNet.Pipeline;
 using SharpTestsEx;
 using Tests.Helpers;
@@ -13,13 +14,15 @@ namespace Tests.Http
 {
     public class HttpLoaderTests
     {
+        private readonly Log _nullLog = (level, exception, message) => {};
+
         [Fact]
         public async Task Should_return_content_on_successful_request()
         {
             var client = new FakeHttpMessageHandler()
                 .Configure(new Uri("http://host/path"), HttpStatusCode.OK, "content")
                 .ToClient();
-            var loader = new HttpLoader(uri => client, Array.Empty<IHttpLoaderPipeline>());
+            var loader = new HttpLoader(uri => client, Array.Empty<IHttpLoaderPipeline>(), _nullLog);
 
             var response = await loader.Get(new Uri("http://host/path"));
             var content = await response.Content.ReadAsStringAsync();
@@ -38,7 +41,7 @@ namespace Tests.Http
             var client = new FakeHttpMessageHandler()
                 .Configure(new Uri("http://host/path"), statusCode, "")
                 .ToClient();
-            var loader = new HttpLoader(uri => client, Array.Empty<IHttpLoaderPipeline>());
+            var loader = new HttpLoader(uri => client, Array.Empty<IHttpLoaderPipeline>(), _nullLog);
 
             // ReSharper disable once PossibleNullReferenceException
             var exception = await Record.ExceptionAsync(() => loader.Get(new Uri("http://host/path")));
@@ -52,7 +55,7 @@ namespace Tests.Http
             var client = new FakeHttpMessageHandler()
                 .Configure(new Uri("http://host/path"), _ => throw new TaskCanceledException())
                 .ToClient();
-            var loader = new HttpLoader(uri => client, Array.Empty<IHttpLoaderPipeline>());
+            var loader = new HttpLoader(uri => client, Array.Empty<IHttpLoaderPipeline>(), _nullLog);
 
             // ReSharper disable once PossibleNullReferenceException
             var exception = await Record.ExceptionAsync(() => loader.Get(new Uri("http://host/path")));
@@ -71,7 +74,7 @@ namespace Tests.Http
                     return new HttpResponseMessage();
                 })
                 .ToClient();
-            var loader = new HttpLoader(uri => client, Array.Empty<IHttpLoaderPipeline>());
+            var loader = new HttpLoader(uri => client, Array.Empty<IHttpLoaderPipeline>(), _nullLog);
 
             await loader.Get(new Uri("http://host/path"));
 
@@ -91,7 +94,7 @@ namespace Tests.Http
                 })
                 .ToClient();
             var pipeline = new LoggingHttpLoaderPipeline(s => log.Add(s));
-            var loader = new HttpLoader(uri => client, new[] {pipeline});
+            var loader = new HttpLoader(uri => client, new[] {pipeline}, _nullLog);
 
             await loader.Get(new Uri("http://host/path"));
 
