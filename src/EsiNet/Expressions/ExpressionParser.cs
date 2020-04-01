aa4d4f8ca4d3a60f9957f1c34b73883028bdd48f
Erik Juhlin <erik.juhlin@tretton37.com>
2020-04-02 01:02:36 +0200

    Extract ConstantParser

--- src/EsiNet/Expressions/ExpressionParser.cs
+++ src/EsiNet/Expressions/ExpressionParser.cs
@@ -205,70 +205,8 @@ namespace EsiNet.Expressions
 
         private static ConstantExpression ParseConstant(ExpressionReader reader)
         {
-            reader.Read(); //Skip '
-
-            var value = new StringBuilder();
-            while (reader.Peek() != -1)
-            {
-                var c = reader.ReadChar();
-                switch (c)
-                {
-                    case '\\':
-                        value.Append(ParseEscapedCharacter(reader));
-                        break;
-                    case '\'':
-                        return new ConstantExpression(value.ToString());
-                    default:
-                        value.Append(c);
-                        break;
-                }
-            }
-
-            throw reader.UnexpectedCharacterException();
+            var constantValue = ConstantParser.Parse(reader);
+            return new ConstantExpression(constantValue);
         }
-
-        private static char ParseEscapedCharacter(ExpressionReader reader)
-        {
-            var c = reader.ReadChar();
-
-            switch (c)
-            {
-                case '\'':
-                case '\\':
-                    return c;
-                case 'b':
-                    return '\b';
-                case 'f':
-                    return '\f';
-                case 'n':
-                    return '\n';
-                case 'r':
-                    return '\r';
-                case 't':
-                    return '\t';
-                case 'u':
-                    return ParseUnicodeCharacter(reader);
-                default:
-                    throw reader.UnexpectedCharacterException();
-            }
-        }
-
-        private static char ParseUnicodeCharacter(ExpressionReader reader)
-        {
-            var code = new StringBuilder();
-            for (var i = 0; i < 4; i++)
-            {
-                var c = reader.ReadChar();
-                if (!IsHexChar(c)) throw reader.UnexpectedCharacterException();
-                code.Append(c);
-            }
-
-            return (char) int.Parse(code.ToString(), NumberStyles.HexNumber);
-        }
-
-        private static bool IsHexChar(char c) =>
-            c >= '0' && c <= '9' ||
-            c >= 'a' && c <= 'f' ||
-            c >= 'A' && c <= 'F';
     }
 }
